449dd9be5268f0d9fdff7a24ccd46ef1569142b7
nzjeux26 <nzjeux18@hotmail.co.nz>
2024-05-07 20:50:34 +1200

    stuff

--- chip.py
+++ chip.py
@@ -5,7 +5,7 @@ import math
 #     Total = 4096 Bytes
 # Opcodes are Big-endian
 
-
+#Fontset
 fontset = bytearray([
     0xF0, 0x90, 0x90, 0x90, 0xF0,  # 0
     0x20, 0x60, 0x20, 0x20, 0x70,  # 1
@@ -24,3 +24,20 @@ fontset = bytearray([
     0xF0, 0x80, 0xF0, 0x80, 0xF0,  # E
     0xF0, 0x80, 0xF0, 0x80, 0x80   # F
 ])
+
+memory = bytearray(4096)
+
+#Registers, 16 GP 8 bit registers start with V and then havea  Hex code
+V = bytearray(16)
+
+# index register and program counter
+I = 0
+#pc starts at memory location 0x200 since the prevous 512bytes are for the interperter 
+pc = 0x200 
+#stack pointer
+sp = 0
+#stack is an array of 16 16bit values
+stack = bytearray(16)
+#keypad is hex, I probably could assign the keys here but will do that in the main.py with the pygame keybaord controls.
+keypad = bytearray(16)
+
